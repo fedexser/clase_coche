0056b07245f3d3de292fd5d00ff4342c9e3b4d82
Fede <fedeserra@gmail.com>
2023-03-24 18:31:06 -0300

    segundo commit funcion suma y clase choce juntas

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
 
 
 public class Main {
+
     public static void main(String[] args) {
+       //ejercicio funcion suma
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        int resultado;
+        resultado=suma(a,b,c);
+        System.out.println("la suma de los numeros es: " + resultado);
+        // ejercicio clase coche
         Coche miCoche = new Coche(3);
         miCoche.addPuerta();
         System.out.println("Mi coche tiene " + miCoche.getNumPuertas() + " puertas.");
+
     }
+
+
+    public static int suma(int a, int b, int c){
+        return a + b + c;
+    }
+
 }
